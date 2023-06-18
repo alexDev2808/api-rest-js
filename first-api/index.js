8daa5577476efd7dadb98e21f51a5903b5c4787b
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-18 16:47:47 -0600

    Utilizando el metodo POST

--- first-api/index.js
+++ first-api/index.js
@@ -1,7 +1,7 @@
 
 const API_KEY = "live_OMB0I4nmHzquFFmeLFuVaL3vFxJ1GYxYiQO1MxgUrAZu1Ko7UL9updsUd97bsBH7";
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=" + API_KEY;
-const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=3&api_key="
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=3&api_key=" + API_KEY;
 
 const img1 = document.querySelector("#img1");
 const img2 = document.querySelector("#img2");
@@ -29,9 +29,27 @@ async function loadFavoritesMichis() {
         spanError.innerHTML = "Hubo un error al cargar Fotos Favoritas: " + response.status;
     } else {
         const data = await response.json();
+        console.log(data)
 
     }
 }
+
+async function saveFavoritesPics() {
+    const response = await fetch(API_URL_FAVORITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                image_id: "12"
+            }
+        )
+    })
+
+    console.log(response);
+}
+
 
 loadRandomMichis();
 loadFavoritesMichis();
