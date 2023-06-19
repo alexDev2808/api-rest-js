c018c53835d37606abb352c759a2660a3e5ae684
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-18 18:12:18 -0600

    Borrando los michis favoritos

--- first-api/index.js
+++ first-api/index.js
@@ -2,6 +2,7 @@
 const API_KEY = "live_OMB0I4nmHzquFFmeLFuVaL3vFxJ1GYxYiQO1MxgUrAZu1Ko7UL9updsUd97bsBH7";
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=" + API_KEY;
 const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?&api_key=" + API_KEY;
+const API_URL_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=${API_KEY}`;
 
 const img1 = document.querySelector("#img1");
 const img2 = document.querySelector("#img2");
@@ -36,17 +37,18 @@ async function loadFavoritesMichis() {
         spanError.innerHTML = "Hubo un error al cargar Fotos Favoritas: " + response.status;
     } else {
         const data = await response.json();
+        const section = document.getElementById('favoritesMichis');
 
-        console.log(data);
+        section.innerHTML = "";
 
         data.forEach(michi => {
-            const section = document.getElementById('favoritesMichis');
             const article = document.createElement('article')
             const img = document.createElement('img');
             const btn = document.createElement('button');
             const btnText = document.createTextNode('Eliminar de favoritos')
 
             btn.appendChild(btnText);
+            btn.onclick = () => deleteFavoriteMichi(michi.id);
             img.src = michi.image.url;
             article.appendChild(img);
             article.appendChild(btn);
@@ -69,9 +71,22 @@ async function saveFavoritesPics(id) {
         )
     })
 
-    console.log(response);
+    loadFavoritesMichis();
+
 }
 
+async function deleteFavoriteMichi(id) {
+    const response = await fetch(API_URL_DELETE(id), {
+        method: 'DELETE'
+    });
+
+    if(response.status !== 200) {
+        spanError.innerHTML = "No se pudo eliminar. Error" + response.status;
+    }else {
+        alert("Michi eliminado!");
+        loadFavoritesMichis();
+    }
+}
 
 loadRandomMichis();
 loadFavoritesMichis();
